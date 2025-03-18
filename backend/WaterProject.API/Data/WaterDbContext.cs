9e9222f2b10174dc9c4284ed88f964de9ab5358b
christiaylor <tay10r@byu.edu>
2025-03-18 16:11:05 -0600

    Initial

---
+++ backend/WaterProject.API/Data/WaterDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+
+namespace WaterProject.API.Data
+{
+    public class WaterDbContext : DbContext
+    {
+        public WaterDbContext(DbContextOptions<WaterDbContext> options) : base(options) 
+        {
+           
+        }
+        public DbSet<Project> Projects { get; set; }
+    }
+}
